a8dab56f77cdd14db025592cd34d2adc8d2e9c4e
sdeli <bgfkszmsdeli@gmail.com>
2019-02-06 19:37:38 +0000

    committed changes in .gitignore

--- mysql/insert-data/insert-test-hotel-data.sql
+++
@@ -1,7 +0,0 @@
-
-call insertArticleCategoryDataIfUnique('animals');
-call insertArticleCategoryDataIfUnique('news');
-call insertArticleCategoryDataIfUnique('politics');
-call insertArticleCategoryDataIfUnique('cars');
-call insertArticleCategoryDataIfUnique('work');
-call insertArticleCategoryDataIfUnique('learning');
--- mysql/source.sql
+++
@@ -1,5 +0,0 @@
-# create db and tables
-source ./dbs/create-db-and-tables.sql
-
-# create procedures
-#source ./procedures/
